b8d3cf922971913d439ac7f7f35c77e3ba3ff719
A <diblo28@yahoo.fr>
2017-05-28 03:27:04 -0400

    Lecture 4: Views

---
+++ FaceDemo/FaceDemo/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  FaceDemo
+//
+//  Created by Abdoulaye Diallo on 6/16/16.
+//  Copyright © 2016 Abdoulaye Diallo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
