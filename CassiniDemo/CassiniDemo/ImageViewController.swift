b2670ed897fe963bdf41f8e9760ee08ccbc4afe7
A <diblo28@yahoo.fr>
2017-06-23 01:48:58 -0400

    Applied Multi threading by implementing a  user initiated dispatch and sent back the UI element to the main queue.

--- CassiniDemo/CassiniDemo/ImageViewController.swift
+++ CassiniDemo/CassiniDemo/ImageViewController.swift
@@ -9,6 +9,49 @@
 import UIKit
 
 class ImageViewController: UIViewController {
-
-   
+    
+    var imageURL: URL? {
+        didSet{
+            image = nil
+            if view.window != nil {
+            fetchImage()
+                
+            }
+        }
+    }
+    
+    func fetchImage(){
+        if let url = imageURL {
+            DispatchQueue.global(qos: .userInitiated).async{ [weak self] in
+                let urlComponents =  try?  Data(contentsOf: url )
+                if let  imageData = urlComponents , url == self?.imageURL {
+                    DispatchQueue.main.async {
+                        self?.image = UIImage(data: imageData)
+                    }
+                }
+            }
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.addSubview(imageView)
+    }
+    
+    
+    
+    
+    private var imageView = UIImageView()
+    private var image: UIImage? {
+        get{
+            return  imageView.image
+        }
+        set{
+            imageView.image = newValue
+            imageView.sizeToFit()
+        }
+    }
+    
+    
+    
+    
 }
